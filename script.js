99a39f21e30a1728c003d290a993571a317ad7ff
Shirin183 <rumayarumiya@gmail.com>
2025-05-06 11:01:31 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let header = document.querySelector(".header");
+
+  
+header.style.width = '1024px'
+header.style.border = '2px solid red'
+header.style.margin = '0 auto'
+nav.style.width = '100%'
+nav.style.height = '71px'
+nav.style.display = 'flex'
+nav.style.justify = 'space-between'
+nav.style.align = 'center'
+div.style.display = 'flex'
+div.style.align = 'center'
+div.style.gap = '6px'
+
